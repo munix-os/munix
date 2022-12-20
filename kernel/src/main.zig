80a679fde6293bb2887c7ed849dd93b20b3247bc
cleanbaja <cleanbaja@protonmail.com>
2022-12-20 01:14:08 +0000

    kernel: implement kernel console (using zig stdlib)

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -1,18 +1,40 @@
+const std = @import("std");
 const limine = @import("limine");
+const logger = std.log.scoped(.main);
 
 pub export var terminal_request: limine.TerminalRequest = .{};
+var bytes: [16 * 4096]u8 = undefined;
+
+pub fn log(
+    comptime level: std.log.Level,
+    comptime scope: anytype,
+    comptime fmt: []const u8,
+    args: anytype,
+) void {
+    var buffer = std.io.fixedBufferStream(&bytes);
+    var writer = buffer.writer();
+
+    switch (level) {
+        .warn => {
+            writer.print("{s}: (\x1b[33mwarn\x1b[0m) ", .{@tagName(scope)}) catch unreachable;
+        },
+        .err => {
+            writer.print("{s}: (\x1b[31merr\x1b[0m) ", .{@tagName(scope)}) catch unreachable;
+        },
+        else => {
+            writer.print("{s}: ", .{@tagName(scope)}) catch unreachable;
+        },
+    }
+
+    writer.print(fmt ++ "\n", args) catch unreachable;
+
+    if (terminal_request.response) |resp| {
+        resp.write(null, buffer.getWritten());
+    }
+}
 
 export fn kernel_entry() callconv(.C) noreturn {
-    // Ensure we got a terminal
-    if (terminal_request.response) |terminal_response| {
-        if (terminal_response.terminal_count < 1) {
-            while (true) {
-                asm volatile ("hlt");
-            }
-        }
-
-        terminal_response.write(null, "Welcome to munix!");
-    }
+    logger.err("Hello from munix!", .{});
 
     while (true) {
         asm volatile ("hlt");
