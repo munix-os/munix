44b9812ff64e4de8227c81453e0cdc5d2f852af7
cleanbaja <cleanbaja@protonmail.com>
2022-12-20 00:53:08 +0000

    kernel: clean up main.zig

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -1,30 +1,20 @@
 const limine = @import("limine");
-const std = @import("std");
 
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent. In Zig, `export var` is what we use.
 pub export var terminal_request: limine.TerminalRequest = .{};
 
-inline fn done() noreturn {
-    while (true) {
-        asm volatile ("hlt");
-    }
-}
-
-// The following will be our kernel's entry point.
-export fn _start() callconv(.C) noreturn {
+export fn kernel_entry() callconv(.C) noreturn {
     // Ensure we got a terminal
     if (terminal_request.response) |terminal_response| {
         if (terminal_response.terminal_count < 1) {
-            done();
+            while (true) {
+                asm volatile ("hlt");
+            }
         }
 
-        // We should now be able to call the Limine terminal to print out
-        // a simple "Hello World" to screen.
-        terminal_response.write(null, "Hello World");
+        terminal_response.write(null, "Welcome to munix!");
     }
 
-    // We're done, just hang...
-    done();
+    while (true) {
+        asm volatile ("hlt");
+    }
 }
